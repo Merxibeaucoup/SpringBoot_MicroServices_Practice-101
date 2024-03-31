60a50e25fe31f7d66854225f7daeda8394b2df3f
Edgar Briandt <ebriandt_6243@email.ric.edu>
2024-03-30 22:58:49 -0400

    discovery done

--- employee-service/src/main/java/com/edgar/employeeservice/services/APIClient.java
+++ employee-service/src/main/java/com/edgar/employeeservice/services/APIClient.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.edgar.employeeservice.requests.DepartmentRequest;
 
-@FeignClient(url = "http://localhost:8444", value="DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8444", value="DEPARTMENT-SERVICE")
+@FeignClient(name = "department-service")
 public interface APIClient {
-	
+
 	@GetMapping("/api/v1/departments/{code}")
-	DepartmentRequest getDepartmentByCode(@PathVariable String code );
+	DepartmentRequest getDepartmentByCode(@PathVariable String code);
 
 }
